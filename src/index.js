4bab0dd915da873f984bb495b84a68e30eccfc87
Petr Degtiarev <ptmp13@gmail.com>
2022-12-09 13:42:49 +0300

    some fix

--- src/index.js
+++ src/index.js
@@ -2,16 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Spaces from './Components/Spaces'
+import SideNav from './Components/Sidenav'
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import { useState, useEffect, useRef } from 'react';
+import { decode as base64_decode, encode as base64_encode } from 'base-64';
+import { 
+  EuiText,
+  EuiProvider,
+  EuiTitle,
+  EuiFlexGroup, 
+  EuiFlexItem, 
+  EuiPageTemplate,
+  EuiIcon,
+  EuiTextColor,
+  EuiButtonIcon
+} from '@elastic/eui';
+import '@elastic/eui/dist/eui_theme_light.css';
+// import Home from "./Pages/Home";
+// import Settings from "./Pages/HomeSpace";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// export default App;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <EuiProvider colorMode="light">
+    {/* <button onClick={() => window.location.reload(false)}>Click to reload!</button> */}
+  <EuiPageTemplate>
+        {/* <EuiPageTemplate.Sidebar sticky="sticky">
+          <SideNav />
+          <EuiText >
+            <p>
+              <EuiTextColor color="subdued">
+                support email: WeblogicAdmin@sportmaster.ru
+              </EuiTextColor>
+            </p>
+          </EuiText>          
+        </EuiPageTemplate.Sidebar>         */}
+        <EuiPageTemplate.Header>
+        <EuiFlexGroup>
+          <EuiFlexItem>        
+            <EuiTitle size="l">
+              <h1><EuiIcon type="logoElastic" size="xl" /> Spaces</h1>
+            </EuiTitle> 
+          </EuiFlexItem>
+          <EuiTextColor color="subdued">
+          <EuiButtonIcon iconType="email"></EuiButtonIcon>  WeblogicAdmin@sportmaster.ru
+          </EuiTextColor>          
+        </EuiFlexGroup>
+        </EuiPageTemplate.Header>
+        <EuiPageTemplate.Section>
+          <Spaces/>
+        </EuiPageTemplate.Section>
+  </EuiPageTemplate>
+  </EuiProvider>        
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
